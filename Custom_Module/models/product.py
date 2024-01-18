2683ba61bd6e3b7217ad7378a16d5ed25d9adaaf
Nikunj2303 <142402711+Nikunj2303@users.noreply.github.com>
2024-01-18 17:08:28 +0530

    Delete Custom_Module directory

--- Custom_Module/models/product.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class ProductProduct(models.Model):
-    _inherit = 'product.product'
-
-    is_case = fields.Boolean(string='Is Case', default=False)
-    is_dial = fields.Boolean(string='Is Dial', default=False)
-    is_bracelet = fields.Boolean(string='Is Bracelet', default=False)
-    is_caliber = fields.Boolean(string='Is Caliber', default=False)
