82106a28d928c3f47c4699578979a7e13848ef59
Nikunj2303 <142402711+Nikunj2303@users.noreply.github.com>
2024-01-19 12:59:42 +0530

    Add files via upload

---
+++ Custom_Module/models/search_term.py
@@ -0,0 +1,8 @@
+
+from odoo import models, fields
+
+class SearchTerm(models.Model):
+    _name = 'search.term'
+    _description = 'Searching Term'
+
+    search_term = fields.Char( string='Searching Term')
