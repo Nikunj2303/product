8981fe7aa56f32097066513dde79edbc657e04c4
Nikunj2303 <142402711+Nikunj2303@users.noreply.github.com>
2024-01-19 17:31:04 +0530

    Delete Custom_Module directory

--- Custom_Module/models/service.py
+++
@@ -1,9 +0,0 @@
-from odoo import api,fields, models
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    vendor_id = fields.Many2one('res.partner', string='Vendor')
-    currency_id = fields.Many2one('res.currency', string='Currency')
-    product_category_id = fields.Many2one('product.category', string='Product Category')
-
