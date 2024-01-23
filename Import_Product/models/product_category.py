fc4a47ca718413728af3c5e09ff1b5ee98d4954b
Nikunj2303 <142402711+Nikunj2303@users.noreply.github.com>
2024-01-23 10:40:30 +0530

    Delete Import_Product directory

--- Import_Product/models/product_category.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-class ProductCategory(models.Model):
-    _inherit = "product.category"
-
-    part_code_ids = fields.Many2many('product.category.part.code', string='Part Codes')
