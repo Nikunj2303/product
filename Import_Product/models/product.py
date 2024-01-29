5e9f5b2b22c453bbb2d2c7cf6e80e3abbba9622b
Nikunj2303 <142402711+Nikunj2303@users.noreply.github.com>
2024-01-29 12:18:37 +0530

    Delete Import_Product directory

--- Import_Product/models/product.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    is_case = fields.Boolean(string='Is Case', default=False)
-    is_dial = fields.Boolean(string='Is Dial', default=False)
-    is_bracelet = fields.Boolean(string='Is Bracelet', default=False)
-    is_caliber = fields.Boolean(string='Is Caliber', default=False)
